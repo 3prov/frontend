c7a710ce464b722349a6e2087a606e9a76ec8772
njajkes <naikes.nikolait@yandex.ru>
2022-09-02 23:16:05 +0300

    revert: revert boundary hoc changes

--- src/components/hoc/Boundary.tsx
+++ src/components/hoc/Boundary.tsx
@@ -16,27 +16,23 @@ type ErrorProps = {
 }
 
 const Error: React.FC<ErrorProps> = ({error, children}) => {
-  return (<>
-    { 
-      error ?    
-      <div className='Error'>
-        <div>Во время загрузки страницы произошла ошибка:</div>
-        <div><code>{error.message}</code></div>
-        <div>Если это поведение было неожиданным для вас, пожалуйста, сообщите нам об этом в телеграм-бот: <a href={'/'}>ссылка скоро будет......</a></div>
-      </div> :
-      { children }
-    }
-  </>)
+  if (!error) return (<>{children}</>)
+  
+  return (
+    <div className='Error'>
+      <div>Во время загрузки страницы произошла ошибка:</div>
+      <div><code>{error.message}</code></div>
+      <div>Если это поведение было неожиданным для вас, пожалуйста, сообщите нам об этом в телеграм-бот: <a href={'/'}>ссылка скоро будет......</a></div>
+    </div>
+  )
 }
 
 const Loader: React.FC<LoaderProps> = ({loading, children}) => {
-  return (<>
-    {
-      loading ?
-      <div className='Loader-container'><span className="loader"/></div> :
-      { children }
-    }
-  </>)
+  if (!loading) return (<>{children}</>)
+  console.log('provided')
+  return (
+    <div className='Loader-container'><span className="loader"/></div>
+  )
 }
 
 const Boundary: React.FC<BoundaryProps> = ({loading, error, children}) => {
