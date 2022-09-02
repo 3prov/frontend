512453a2fd5234a8fc42ab6e1dca82c698a30b01
njajkes <naikes.nikolait@yandex.ru>
2022-09-02 22:38:00 +0300

    app: provide store

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Write from './components/pages/Write';
 import './App.css'
 import Exam from './components/pages/Exam';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/write/:id' element={<Write />} />
-        <Route path='/evaluate/:id' element={<Exam />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/write/:id' element={<Write />} />
+          <Route path='/evaluate/:id' element={<Exam />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
