dda338aad939a05495b8df4119db5dc0c8f734a8
njajkes <naikes.nikolait@yandex.ru>
2022-09-04 02:34:32 +0300

    server: fix proxing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use('/api/*', createProxyMiddleware({target: 'http://localhost:8023', changeOrigin: true}))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
