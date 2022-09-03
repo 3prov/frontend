58e8752215a0cebd2b745a29d80cf5d9ba7d67bb
njajkes <naikes.nikolait@yandex.ru>
2022-09-03 22:54:28 +0300

    server: prod server is done

--- src/api/rootApi.ts
+++ src/api/rootApi.ts
@@ -1,5 +1,5 @@
 export default class Api {
-  static BASE_URL = '/api/v1/'
+  static BASE_URL = process.env.NODE_ENV === 'development' ? '/api/v1/' : 'http://localhost:8023/api/v1/' 
   static Headers = {
     'Content-Type': 'application/json'
   }
