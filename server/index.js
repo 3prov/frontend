cdb7913ebd413de23b910e97c7e2b98ae57a753a
njajkes <naikes.nikolait@yandex.ru>
2022-09-05 01:47:56 +0300

    server: add compression

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
+const compression = require('compression');
 
+app.use('/api/*', createProxyMiddleware({target: 'http://localhost:8023', changeOrigin: true}));
+
+app.use(compression({chunkSize: 16256}))
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.use('/api/*', createProxyMiddleware({target: 'http://localhost:8023', changeOrigin: true}))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
